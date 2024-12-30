e0ed1111270e2e213eaadf169d21ff217df931d7
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2024-12-30 20:08:48 +0900

    Feat: Post List page & Post Single page (unworked)

---
+++ src/app/(post)/post/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AlumiLink_Post_Single_Page() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-start items-start gap-8">
+      <div className=""></div>
+      <div className=""></div>
+
+      <div className=""></div>
+    </div>
+  );
+}
