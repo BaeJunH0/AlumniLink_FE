1859d756d86cb6a1b4f84ea22d12921982e17258
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-04 01:26:04 +0900

    Feat: CustomLayout Header design changed ( sticky -> fixed )

--- src/shared/layout/ui/CustomLayout.tsx
+++ src/shared/layout/ui/CustomLayout.tsx
@@ -8,13 +8,15 @@ type T_CustomLayout = {
 
 function CustomLayout({ isHeader, children }: T_CustomLayout) {
   return (
-    <div
-      className={`relative w-screen bg-gradient-to-r from-[#D3CCE3] to-[#E9E4F0] ${
-        !isHeader && "flex justify-center items-center"
-      }`}
-    >
+    <div className="relative w-screen bg-gradient-to-r from-[#D3CCE3] to-[#E9E4F0] overflow-x-hidden">
       {isHeader && <Header />}
-      <div className="px-32 py-10">{children}</div>
+      <div
+        className={` w-full min-h-screen ${
+          !isHeader ? "flex justify-center items-center pt-0" : "pt-28"
+        }`}
+      >
+        <div className="px-32 py-4">{children}</div>
+      </div>
     </div>
   );
 }
